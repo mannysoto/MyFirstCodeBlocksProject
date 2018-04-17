fe0e8ffb92893c928ff01c3aab30826eec6334de
Manny Soto <mannysoto@yahoo.com>
2018-04-17 16:20:07 -0600

    Commiting...

--- main.cpp
+++ main.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Hello Manny!" << endl;
     cout << "Hello Again" << endl;
     cout << "The cat made me say it!!" << endl;
+    cout << "The dog is misbehaving!!" << endl;
     return 0;
 }
